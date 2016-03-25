a1c42364d39d2a97422f36a2563dd84d90fe6e03
Jack Phoenix <jack@countervandalism.net>
2016-03-25 12:05:16 +0200

    s/four spaces/one tab/
    Change-Id: Iddf7a576571a3aec145a5676c56333b16fd814bf

--- api/CommentLatestID.api.php
+++ api/CommentLatestID.api.php
@@ -2,21 +2,21 @@
 
 class CommentLatestIdAPI extends ApiBase {
 
-    public function execute() {
-        $pageID = $this->getMain()->getVal( 'pageID' );
+	public function execute() {
+		$pageID = $this->getMain()->getVal( 'pageID' );
 
-        $commentsPage = new CommentsPage( $pageID, RequestContext::getMain() );
+		$commentsPage = new CommentsPage( $pageID, RequestContext::getMain() );
 
-        $result = $this->getResult();
-        $result->addValue( $this->getModuleName(), 'id', $commentsPage->getLatestCommentID() );
-    }
+		$result = $this->getResult();
+		$result->addValue( $this->getModuleName(), 'id', $commentsPage->getLatestCommentID() );
+	}
 
-    public function getAllowedParams() {
-        return array(
-            'pageID' => array(
-                ApiBase::PARAM_REQUIRED => true,
-                ApiBase::PARAM_TYPE => 'int'
-            )
-        );
-    }
+	public function getAllowedParams() {
+		return array(
+			'pageID' => array(
+				ApiBase::PARAM_REQUIRED => true,
+				ApiBase::PARAM_TYPE => 'int'
+			)
+		);
+	}
 }
--- api/CommentList.api.php
+++ api/CommentList.api.php
@@ -2,43 +2,43 @@
 
 class CommentListAPI extends ApiBase {
 
-    public function execute() {
-        $commentsPage = new CommentsPage( $this->getMain()->getVal( 'pageID' ), RequestContext::getMain() );
-        $commentsPage->orderBy = $this->getMain()->getVal( 'order' );
-        $commentsPage->currentPagerPage = $this->getMain()->getVal( 'pagerPage' );
+	public function execute() {
+		$commentsPage = new CommentsPage( $this->getMain()->getVal( 'pageID' ), RequestContext::getMain() );
+		$commentsPage->orderBy = $this->getMain()->getVal( 'order' );
+		$commentsPage->currentPagerPage = $this->getMain()->getVal( 'pagerPage' );
 
-        $output = '';
-        if ( $this->getMain()->getVal( 'showForm' ) ) {
-            $output .= $commentsPage->displayOrderForm();
-        }
-        $output .= $commentsPage->display();
-        if ( $this->getMain()->getVal( 'showForm' ) ) {
-            $output .= $commentsPage->displayForm();
-        }
+		$output = '';
+		if ( $this->getMain()->getVal( 'showForm' ) ) {
+			$output .= $commentsPage->displayOrderForm();
+		}
+		$output .= $commentsPage->display();
+		if ( $this->getMain()->getVal( 'showForm' ) ) {
+			$output .= $commentsPage->displayForm();
+		}
 
-        $result = $this->getResult();
-        $result->addValue( $this->getModuleName(), 'html', $output );
-        return true;
-    }
+		$result = $this->getResult();
+		$result->addValue( $this->getModuleName(), 'html', $output );
+		return true;
+	}
 
-    public function getAllowedParams() {
-        return array(
-            'pageID' => array(
-                ApiBase::PARAM_REQUIRED => true,
-                ApiBase::PARAM_TYPE => 'integer'
-            ),
-            'order' => array(
-                ApiBase::PARAM_REQUIRED => true,
-                ApiBase::PARAM_TYPE => 'boolean'
-            ),
-            'pagerPage' => array(
-                ApiBase::PARAM_REQUIRED => true,
-                ApiBase::PARAM_TYPE => 'integer'
-            ),
-            'showForm' => array(
-                ApiBase::PARAM_REQUIRED => false,
-                ApiBase::PARAM_TYPE => 'integer'
-            )
-        );
-    }
+	public function getAllowedParams() {
+		return array(
+			'pageID' => array(
+				ApiBase::PARAM_REQUIRED => true,
+				ApiBase::PARAM_TYPE => 'integer'
+			),
+			'order' => array(
+				ApiBase::PARAM_REQUIRED => true,
+				ApiBase::PARAM_TYPE => 'boolean'
+			),
+			'pagerPage' => array(
+				ApiBase::PARAM_REQUIRED => true,
+				ApiBase::PARAM_TYPE => 'integer'
+			),
+			'showForm' => array(
+				ApiBase::PARAM_REQUIRED => false,
+				ApiBase::PARAM_TYPE => 'integer'
+			)
+		);
+	}
 }
