507c582a10634eb8985c9b7d264d26a382452102
Jack Phoenix <jack@countervandalism.net>
2017-06-28 23:04:59 +0300

    [WIP] Version 4.4.0: redesigned per Samantha's mockup
    Needs quite a lot of PHP love and some CSS love.
    
    CSS/LESS will also need cleanup, right now there are two files -- one of
    each --, the old .css file is needed for the proper formatting of the
    listing of pre-existing comments whereas the new LESS file is needed for
    the comment submission box.
    
    What works:
    * submitting a comment
    * replying to a comment
    * previewing a reply (new!)
    
    Needs more testing:
    * how does the form look when the user doesn't have the 'comment' user right?
    * and how does it behave when commenting is restricted to a subset of users _or_ the current user is blocked?
    
    Known to be broken:
    * character count isn't reset back to 0/1500 after submitting a comment
    * clicking on the "cancel" button makes the "Reply to <username>" text (#replyto) disappear only once
    * a lot of other stuff I didn't even think of testing, probably
    
    TODO:
    * Extract the comment submitting into the form handler module for
    semantics
    * Use jquery.byteLimit module for the byte limit
    
    Based on: https://samanthanguyen.github.io/mediawiki-demo-Comments/
    
    Change-Id: Id23c85dae04678bc17c9c77c7267d5defb7be01a

---
+++ resources/js/comments-form-handler.js
@@ -0,0 +1,96 @@
+/**
+ * handles all functions related to the comment
+ * form, such as previewing and the tabs
+ *
+ * @file
+ */
+( function ( $, mw ) {
+
+var commentFormHandler = {
+	/**
+	 * Preview the contents of the <textarea> (more accurately the
+	 * wikitext given to this function) and show the parsed HTML
+	 * output in the given element.
+	 *
+	 * @param text {string} Wikitext to be sent to the API for parsing
+	 * @param targetElement {string} ID of the element where
+	 * the preview text should be inserted
+	 */
+	preview: function ( text, targetElement ) {
+		( new mw.Api() ).get( {
+			action: 'parse',
+			text: text,
+			contentmodel: 'wikitext',
+			disablelimitreport: true // don't need the NewPP report stuff
+		} ).done( function ( data ) {
+			$( '#' + targetElement ).html( data.parse.text['*'] ); //.replace( /\<!\--(.*)-->/, '' );
+		} ).fail( function () {
+			$( '#' + targetElement ).text( mw.msg( 'comments-preview-failed' ) );
+		} );
+	},
+
+	/**
+	 * build the write and preview tab needed
+	 * for the comment form
+	 */
+	buildTabs: function () {
+		var formTabs = {
+			write: new OO.ui.TabPanelLayout( 'write', {
+				label: mw.msg( 'comments-tab-write' ),
+			} ),
+			preview: new OO.ui.TabPanelLayout( 'preview', {
+				label: mw.msg( 'comments-tab-preview' )
+			} )
+		};
+
+		formTabs.write.$content.append(
+			$( '#comment-form' ).$element
+		);
+
+		formTabs.preview.$content.append(
+			commentFormHandler.preview(
+				$( '#comment-form' ).val(),
+				$( this ).attr( 'data-linked-element' )
+			).$element
+		);
+
+		var formIndex = new OO.ui.IndexLayout();
+		formIndex.addTabPanels( [ formTabs.write, formTabs.preview ] );
+		$( '.comments-body' ).html( formIndex.$element );
+	},
+
+	/**
+	 * build the buttons that let the user
+	 * either submit or cancel their comment.
+	 */
+	buildButtons: function() {
+		var formButtons = {
+			submit: new OO.ui.ButtonInputWidget( {
+				label: mw.msg( 'comments-post' ),
+				icon: 'speechBubbleAdd',
+				type: 'submit',
+				flags: [ 'primary', 'progressive' ]
+			} ),
+			cancel: new OO.ui.ButtonInputWidget( {
+				label: mw.msg( 'comments-cancel-reply' ),
+				frameless: true,
+				flags: [ 'destructive' ]
+			} )
+		};
+
+		// decide what buttons to show
+		if ( $( '#replyto' ).text() !== '' ) {
+			var buttonGroup = new OO.ui.ButtonGroupWidget( {
+				items: [
+					formButtons.submit,
+					formButtons.cancel
+				]
+			} );
+			return buttonGroup;
+		} else {
+			return formButtons.submit;
+		}
+	}
+};
+
+}( jQuery, mediaWiki ) );
