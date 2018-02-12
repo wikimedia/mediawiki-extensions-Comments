1b6cea7d22c023720a953b0740b5fff2bbc2272d
Jayprakash12345 <0freerunning@gmail.com>
2018-02-12 17:47:26 +0000

    Update at-ease calls in extensions
    Bug: T187037
    Change-Id: Ied4710830cc81480884179587a2be62b3fc43546

--- includes/Comment.class.php
+++ includes/Comment.class.php
@@ -284,9 +284,9 @@ class Comment extends ContextSource {
 		$dbw = wfGetDB( DB_MASTER );
 		$context = RequestContext::getMain();
 
-		MediaWiki\suppressWarnings();
+		Wikimedia\suppressWarnings();
 		$commentDate = date( 'Y-m-d H:i:s' );
-		MediaWiki\restoreWarnings();
+		Wikimedia\restoreWarnings();
 		$dbw->insert(
 			'Comments',
 			array(
@@ -392,9 +392,9 @@ class Comment extends ContextSource {
 			$value = 0;
 		}
 
-		MediaWiki\suppressWarnings();
+		Wikimedia\suppressWarnings();
 		$commentDate = date( 'Y-m-d H:i:s' );
-		MediaWiki\restoreWarnings();
+		Wikimedia\restoreWarnings();
 
 		if ( $this->currentVote === false ) { // no vote, insert
 			$dbw->insert(
@@ -744,13 +744,13 @@ class Comment extends ContextSource {
 		$output .= "{$commentPoster}";
 		$output .= "<span class=\"c-user-level\">{$commentPosterLevel}</span> {$blockLink}" . "\n";
 
-		MediaWiki\suppressWarnings(); // E_STRICT bitches about strtotime()
+		Wikimedia\suppressWarnings(); // E_STRICT bitches about strtotime()
 		$output .= '<div class="c-time">' .
 			wfMessage(
 				'comments-time-ago',
 				CommentFunctions::getTimeAgo( strtotime( $this->date ) )
 			)->parse() . '</div>' . "\n";
-		MediaWiki\restoreWarnings();
+		Wikimedia\restoreWarnings();
 
 		$output .= '<div class="c-score">' . "\n";
 		$output .= $this->getScoreHTML();
