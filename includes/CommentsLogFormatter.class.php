8d1daa3518301ace36afb5c069265111e186865e
SamanthaNguyen <samanthanguyen1116@gmail.com>
2018-02-26 19:59:51 -0600

    DNM (till 1.31 stable release): Make CommentsLogFormatter extend WikitextLogFormatter
    We can remove this code duplication by having CommentsLogFormatter
    extend the new WikitextLogFormatter introduced in 1.31.
    
    Change-Id: I91536b64859f10118cf3b69fd76bec64cb64ee6e

--- includes/CommentsLogFormatter.class.php
+++ includes/CommentsLogFormatter.class.php
@@ -5,32 +5,7 @@
  * @file
  * @date 28 July 2013
  */
-class CommentsLogFormatter extends LogFormatter {
-	/**
-	 * Gets the log action, including username.
-	 *
-	 * This is a copy of LogFormatter::getActionText() with one "escaped"
-	 * swapped to parse; no other changes here!
-	 *
-	 * @return string HTML
-	 */
-	public function getActionText() {
-		if ( $this->canView( LogPage::DELETED_ACTION ) ) {
-			$element = $this->getActionMessage();
-			if ( $element instanceof Message ) {
-				$element = $this->plaintext ? $element->text() : $element->parse(); // <-- here's the change!
-			}
-			if ( $this->entry->isDeleted( LogPage::DELETED_ACTION ) ) {
-				$element = $this->styleRestricedElement( $element );
-			}
-		} else {
-			$performer = $this->getPerformerElement() . $this->msg( 'word-separator' )->text();
-			$element = $performer . $this->getRestrictedElement( 'rev-deleted-event' );
-		}
-
-		return $element;
-	}
-
+class CommentsLogFormatter extends WikitextLogFormatter {
 	/**
 	 * Formats parameters intented for action message from
 	 * array of all parameters. There are three hardcoded
