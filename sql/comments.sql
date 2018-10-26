b9cac04200a6124e3ffaaaf2b8cc76511a7dc123
Paladox <thomasmulhall410@yahoo.com>
2018-10-26 16:02:36 +0000

    Use "CREATE TABLE IF NOT EXISTS" instead of "CREATE TABLE"
    This is to prevent it erroring out if the table exists.
    
    MW supports mysql 5.5+ so this should be ok.
    
    Change-Id: I1003b7b59d84dc5267a39a4acc07f0e5b5a63ebc

--- sql/comments.sql
+++ sql/comments.sql
@@ -1,5 +1,5 @@
 -- MySQL/SQLite schema for the Comments extension
-CREATE TABLE /*_*/Comments (
+CREATE TABLE IF NOT EXISTS /*_*/Comments (
   CommentID int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   Comment_Page_ID int(11) NOT NULL default 0,
   Comment_user_id int(11) NOT NULL default 0,
@@ -14,7 +14,7 @@ CREATE INDEX /*i*/comment_page_id_index ON /*_*/Comments (Comment_Page_ID);
 CREATE INDEX /*i*/wiki_user_id ON /*_*/Comments (Comment_user_id);
 CREATE INDEX /*i*/wiki_user_name ON /*_*/Comments (Comment_Username);
 
-CREATE TABLE /*_*/Comments_Vote (
+CREATE TABLE IF NOT EXISTS /*_*/Comments_Vote (
   Comment_Vote_ID int(11) NOT NULL default 0,
   Comment_Vote_user_id int(11) NOT NULL default 0,
   Comment_Vote_Username varchar(200) NOT NULL default '',
@@ -27,7 +27,7 @@ CREATE UNIQUE INDEX /*i*/Comments_Vote_user_id_index ON /*_*/Comments_Vote (Comm
 CREATE INDEX /*i*/Comment_Vote_Score ON /*_*/Comments_Vote (Comment_Vote_Score);
 CREATE INDEX /*i*/Comment_Vote_user_id ON /*_*/Comments_Vote (Comment_Vote_user_id);
 
-CREATE TABLE /*_*/Comments_block (
+CREATE TABLE IF NOT EXISTS /*_*/Comments_block (
   cb_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   cb_user_id int(5) NOT NULL default 0,
   cb_user_name varchar(255) NOT NULL default '',
