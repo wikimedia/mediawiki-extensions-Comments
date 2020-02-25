4fb37fc25af66b57c7be65f020cc5250abac63a6
Jack Phoenix <ashley@uncyclomedia.co>
2020-02-25 20:25:52 +0200

    Ostrzyciel's fix for anons who may not have performed any other actions than commenting or getting their comments blocked
    Bug: T227345
    Change-Id: I1285818d222eb60e7ba464a11b6806b26bd99c00
    Follow-up: 7c9a27d996aa27b3cb7669c28b96784e19ff3dd6

--- maintenance/migrateOldCommentsBlockUserColumnsToActor.php
+++ maintenance/migrateOldCommentsBlockUserColumnsToActor.php
@@ -51,16 +51,56 @@ class MigrateOldCommentsBlockUserColumnsToActor extends LoggedUpdateMaintenance 
 			// Why is this loop here? Because Postgres was being weird, that's why.
 			return true;
 		}
+
+		// Copypasted code from AJAXPoll's migration script, written by Ostrzyciel
+		// Find missing anonymous actors and insert them to the actor table
+		// Do not attempt doing it with an insertSelect, it's apparently incompatible with postgres
+		$res = $dbw->select(
+			[
+				'Comments_block',
+				'actor'
+			],
+			[ 'cb_user_name_blocked' ],
+			[
+				// comment ignoring is something available only for registered users;
+				// Special:CommentIgnoreList requires logging in, _but_ this means that
+				// while anons may never _block_ comments, anons may have _their_ comments
+				// blocked!
+				// Hence why we are ignoring the cb_user_name/cb_user_id fields here: it
+				// should *never* be possible for cb_user_id to be 0!
+				'cb_user_id_blocked' => 0
+				'actor_id IS NULL'
+			],
+			__METHOD__,
+			[ 'DISTINCT' ],
+			[
+				'actor' => [ 'LEFT JOIN', [ 'actor_name = cb_user_name_blocked' ] ]
+			]
+		);
+
+		$toInsert = [];
+
+		foreach ( $res as $row ) {
+			$toInsert[] = [ 'actor_name' => $row->cb_user_name_blocked ];
+		}
+
+		if ( !empty( $toInsert ) ) {
+			$dbw->insert( 'actor', $toInsert, __METHOD__ );
+		}
+		// End copypasta
+
 		$dbw->query(
 			// @codingStandardsIgnoreLine
 			"UPDATE {$dbw->tableName( 'Comments_block' )} SET cb_actor=(SELECT actor_id FROM {$dbw->tableName( 'actor' )} WHERE actor_user=cb_user_id AND actor_name=cb_user_name)",
 			__METHOD__
 		);
+
 		$dbw->query(
 			// @codingStandardsIgnoreLine
 			"UPDATE {$dbw->tableName( 'Comments_block' )} SET cb_actor_blocked=(SELECT actor_id FROM {$dbw->tableName( 'actor' )} WHERE actor_user=cb_user_id_blocked AND actor_name=cb_user_name_blocked)",
 			__METHOD__
 		);
+
 		return true;
 	}
 }
